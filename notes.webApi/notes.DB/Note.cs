5b0cc60e7c0882ec99e81f605f89ad4f6ddd62a0
Admin <Admin@DESKTOP-38N1J1R>
2021-03-04 22:32:23 +0200

    Create WebApi

---
+++ notes.webApi/notes.DB/Note.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace notes.DB
+{
+    public class Note
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Value { get; set; }
+
+    }
+}
---
+++ notes.webApi/notes.webApi/Controllers/NotesController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using notes.Core;
+using notes.DB;
+
+namespace notes.webApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class NotesController : ControllerBase
+    {
+
+        private readonly ILogger<NotesController> _logger;
+        private readonly INotesServices _notesServices;
+
+        public NotesController(ILogger<NotesController> logger, INotesServices notesServices)
+        {
+            _logger = logger;
+            _notesServices = notesServices;
+        }
+
+        [HttpGet]
+        public IActionResult GetNotes()
+        {
+            return Ok(_notesServices.GetNotes());
+        }
+
+        [HttpGet("{id}", Name = "GetNote")]
+        public IActionResult GetNote(int id)
+        {
+            return Ok(_notesServices.GetNote(id));
+        }
+
+        [HttpPost]
+        public IActionResult CreateNote(Note note)
+        {
+            var newNote = _notesServices.CreateNote(note);
+            return CreatedAtRoute("GetNote", new {newNote.Id}, newNote);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteNote(int id)
+        {
+            _notesServices.DeleteNote(id);
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult EditNote([FromBody] Note note)
+        {
+            _notesServices.EditNote(note);
+            return Ok();
+        }
+    }
+}
